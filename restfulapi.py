abc72072b90f2cb5767522f8663b412809b19d71
Alexander Grothe <seahawk1986@hotmail.com>
2011-07-28 18:24:11 +0200

    TODO changes

--- restfulapi.py
+++ restfulapi.py
@@ -17,7 +17,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# TODO: list recordings
+# TODO: POST and DELETE commands
 
 import urllib
 import urllib2
