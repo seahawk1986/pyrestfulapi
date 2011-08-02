03a7793d5b4013d686e9563c8350c9653c6aa9c2
Alexander Grothe <seahawk1986@hotmail.com>
2011-08-02 12:11:50 +0200

    added basic OSD handling and epg search, still a lot of work to do

--- restfulapi.py
+++ restfulapi.py
@@ -17,21 +17,23 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-# TODO: POST, PUT and DELETE commands (timers, searchtimer, search)
+# TODO: POST, PUT and DELETE commands (searchtimer, search)
 #       ERROR handling (connection, double timers etc.)
-#       EPG search
+#       EPG search -> HOWTO search quoted strings, regex ?
 #       searchtimer
 #       download channel images
 #       infos (used versions and plugins)
-#       OSD... maybe
+#       OSD... maybe - seems quite complex for adapted 
 
 import urllib
 import urllib2
 from lxml import etree
 import httplib
 import simplejson
+import re
 
 class HttpClient:
+    ''' Collection of all needed HTTP-Request methods'''
     def __init__(self):
         redirect_handler= urllib2.HTTPRedirectHandler()
         self._opener = urllib2.build_opener(redirect_handler)
@@ -41,6 +43,13 @@ class HttpClient:
 
     def POST(self, url,parameters):
         return self._opener.open(url, urllib.urlencode(parameters)).read()
+
+    def POST_raw(self, url, parameters):
+        return self._opener.open(url, parameters).read()
+
+    def POST_json(self, url, parameters):
+        print simplejson.dumps(parameters)
+        return self._opener.open(url, simplejson.dumps(parameters)).read()
 
     def PUT(self, url="",body='',header='HTTP/1.1'):
         opener = urllib2.build_opener(urllib2.HTTPHandler)
@@ -59,6 +68,7 @@ class HttpClient:
 
 
 class RestfulAPI:
+    ''' Provides access to functions of VDR's restfulapi-plugin'''
 
     def __init__(self,ip="127.0.0.1", port="8002"):
         self.ip = ip
@@ -69,33 +79,37 @@ class RestfulAPI:
         self.HTTP = HttpClient()
         self.JSON = simplejson.JSONDecoder()
 
-    def get_elements(self,req_url):
-            xmltree = etree.fromstring(urllib2.urlopen(req_url).read())
-            raw_elements = xmltree.getchildren()
-            return raw_elements
+    def get_elements(self,req_url): 
+        '''extracts elements from xml source'''
+        xmltree = etree.fromstring(urllib2.urlopen(req_url).read())
+        raw_elements = xmltree.getchildren()
+        return raw_elements
 
-    def get_entrys(self,raw_elements,xml_str=None,keyword=None):    
-            entry_list = []    
-            for entry in raw_elements:
-                if entry.tag == "{http://www.domain.org/restfulapi/2011/%s-xml}%s"%(xml_str,keyword):
-                    paramdict = {}
-                    if len(entry.getchildren())>0:
-                        for param in entry.getchildren():
-                            paramdict[param.values()[0]]=param.text
-                        # stores elements in a list, as list containing [ {Properties as dict} ]
-                        entry_list.append(paramdict)
-                    else:
-                        entry_list.append(entry.text)
-                        
-                if entry.tag == "{http://www.domain.org/restfulapi/2011/%s-xml}count"%(xml_str):
-                    count = int(entry.text)
-                    #print "Counted "+entry.text+" elements."                         
-                if entry.tag == "{http://www.domain.org/restfulapi/2011/%s-xml}total"%(xml_str):
-                    total = int(entry.text)
-                    #print "Total "+entry.text+" elements."
-            return entry_list, count, total           
+    def get_entrys(self,raw_elements,xml_str=None,keyword=None):   
+        '''creates a list of elements matching keyword, number of elements, total elements
+           as helpe fuction for get_list'''
+        entry_list = []    
+        for entry in raw_elements:
+            if entry.tag == "{http://www.domain.org/restfulapi/2011/%s-xml}%s"%(xml_str,keyword):
+                paramdict = {}
+                if len(entry.getchildren())>0:
+                    for param in entry.getchildren():
+                        paramdict[param.values()[0]]=param.text
+                    # stores elements in a list, as list containing [ {Properties as dict} ]
+                    entry_list.append(paramdict)
+                else:
+                    entry_list.append(entry.text)
+                    
+            if entry.tag == "{http://www.domain.org/restfulapi/2011/%s-xml}count"%(xml_str):
+                count = int(entry.text)
+                #print "Counted "+entry.text+" elements."                         
+            if entry.tag == "{http://www.domain.org/restfulapi/2011/%s-xml}total"%(xml_str):
+                total = int(entry.text)
+                #print "Total "+entry.text+" elements."
+        return entry_list, count, total           
 
     def get_list(self,cat=None,arg="",start=0,limit=0):
+        '''returns a touple of elements as (list, count, total)'''
 
         if cat == "channels":
             req_url = "%s/%s.xml?start=%s&limit=%s"%(self.base_url,cat,start,limit)
@@ -146,6 +160,18 @@ class RestfulAPI:
             count = 0
             total = 0
         return channel_list, count, total
+
+    def get_channel_by_number(self, number=None):
+        ret_ch = None
+        if number != None:
+            number = int(number)
+            channel_list,count,total = self.get_category(cat="channels")
+            for channel in channel_list:
+                if channel['number'] == number:
+                    ret_ch = channel
+                    break
+        return ret_ch
+
                 
     def get_groups(self,start=0,limit=0):
         channel_list, count, total = self.get_list(cat="groups",start=start,limit=limit)
@@ -181,10 +207,10 @@ class RestfulAPI:
     def delete_recording(self, rec_number=None):
         if rec_number != None:
             url = "%s/recording/%s"%(self.base_url,rec_number)
-            ignored_html = self.HTTP.DELETE(url)
+            return self.HTTP.DELETE(url)
         
 
-    def get_info(self):
+    def get_playing(self):
         cat = "info"
         req_url = "%s/%s.xml"%(self.base_url,cat)
         xml_str = "info"
@@ -197,6 +223,23 @@ class RestfulAPI:
             if entry.tag == "{http://www.domain.org/restfulapi/2011/%s-xml}%s"%(xml_str,"video"):
                 channel = None
                 video = entry.text
+        return channel, video
+
+    def get_info(self):
+        cat = "info"
+        req_url = "%s/%s.json"%(self.base_url,cat)
+        xml_str = "info"
+        keyword = "channel"
+        info = self.JSON.decode(self.HTTP.GET(req_url))
+        try:
+            channel = info['channel']
+        except:
+            channel = None
+        try:
+            video = info['video']
+        except:
+            video = None
+
         return channel, video
 
     def get_channel_image_url(self, channel_id=None, name=None):
@@ -214,12 +257,20 @@ class RestfulAPI:
             if channel_id != None:
                 if (channel['channel_id'] == channel_id) or (channel['name'] == name):
                     client = HttpClient()
-                    print "%s/remote/switch/%s"%(self.base_url,channel_id)
                     ignored_html = self.HTTP.POST("%s/remote/switch/%s"%(self.base_url,channel_id), {})
+        return "Switched to %s"%(channel_id)
 
     def send_remote(self, command):
-        '''"Up", "Down", "Menu", "Ok", "Back", "Left", "Right", "Red", "Green", "Yellow", "Blue", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Info", "Play", "Pause", "Stop", "Record", "FastFwd", "FastRew", "Next", "Prev", "Power", "ChanUp", "ChanDn", "ChanPrev", "VolUp", "VolDn", "Mute", "Audio", "Subtitles", "Schedule", "Channels", "Timers", "Recordings", "Setup", "Commands", "User0", "User1", "User2", "User3", "User4", "User5", "User6", "User7", "User8", "User9", "None", "Kbd"'''
-        valid_commands=["Up", "Down", "Menu", "Ok", "Back", "Left", "Right", "Red", "Green", "Yellow", "Blue", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Info", "Play", "Pause", "Stop", "Record", "FastFwd", "FastRew", "Next", "Prev", "Power", "ChanUp", "ChanDn", "ChanPrev", "VolUp", "VolDn", "Mute", "Audio", "Subtitles", "Schedule", "Channels", "Timers", "Recordings", "Setup", "Commands", "User0", "User1", "User2", "User3", "User4", "User5", "User6", "User7", "User8", "User9", "None", "Kbd"]
+        '''Supported Commands: "Up", "Down", "Menu", "Ok", "Back", "Left", "Right", "Red", "Green", "Yellow", "Blue",
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Info", "Play", "Pause", "Stop", "Record",
+            "FastFwd", "FastRew", "Next", "Prev", "Power", "ChanUp", "ChanDn", "ChanPrev", "VolUp", "VolDn",
+            "Mute", "Audio", "Subtitles", "Schedule", "Channels", "Timers", "Recordings", "Setup", "Commands",
+            "User0", "User1", "User2", "User3", "User4", "User5", "User6", "User7", "User8", "User9", "None", "Kbd"'''
+        valid_commands=["Up", "Down", "Menu", "Ok", "Back", "Left", "Right", "Red", "Green", "Yellow", "Blue",
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Info", "Play", "Pause", "Stop", "Record",
+            "FastFwd", "FastRew", "Next", "Prev", "Power", "ChanUp", "ChanDn", "ChanPrev", "VolUp", "VolDn",
+            "Mute", "Audio", "Subtitles", "Schedule", "Channels", "Timers", "Recordings", "Setup", "Commands",
+            "User0", "User1", "User2", "User3", "User4", "User5", "User6", "User7", "User8", "User9", "None", "Kbd"]
         client = HttpClient()
         ignored_html = self.HTTP.POST("%s/remote/%s"%(self.base_url,command), {})
 
@@ -227,6 +278,8 @@ class RestfulAPI:
         ignored_html = self.HTTP.POST("%s/timers"%(self.base_url), params)
 
     def create_timer_dict(self, filename='Test', flags='1', start='1200', stop='1215', day='2099-08-08',channel='T-8468-12290-32',weekdays='-------'):
+        '''creates a dictionary containung all needed elements to create a timer with create_timer(dictionary)
+            please take care to provide ALL needed elements'''
         timerdict = {}
         timerdict['file']=filename
         timerdict['flags']=flags
@@ -238,6 +291,7 @@ class RestfulAPI:
         return timerdict
 
     def update_timer(self, timer_id, params={}):
+        '''updates parameters of an existing timer, needs 'timer_id' which is called 'id' in timer lists aquired by get_timers()'''
         url = "%s/timers.json"%self.base_url
         params['timer_id']=timer_id
         json = simplejson.dumps(params)
@@ -252,7 +306,17 @@ class RestfulAPI:
         '''get tuple of category entries as (dictionary, number of found entries, total number)
            allowed categories: channels, channel [arg="<channel name>"], groups, group [arg="<groupname>"],
            timers, timer [arg="<timer name>"], recordings
-           use start=n to begin with n-th entry and limit=m to recieve m entries.'''
+           use start=n to begin with n-th entry and limit=m to recieve m entries.
+           This function is supposed to replace get_elements(), get_entrys() and get_list() some day'''
+        try:
+            start = int(start)
+        except:
+            print "start has to be an integer"
+
+        try:
+            limit = int(limit)
+        except:
+            print "limit has to be an integer"
 
         if cat == "channels":
             req_url = "%s/%s.json?start=%s&limit=%s"%(self.base_url,cat,start,limit)
@@ -286,10 +350,80 @@ class RestfulAPI:
             req_url = "%s/recordings.json"%(self.base_url)
             xml_str = "recordings"
             keyword = "recording"
-        
-        elementlist = []
+
         json = self.JSON.decode(self.HTTP.GET(req_url))
-        return json['%s'%xml_str], json['count'], json['total']
+        return json[xml_str], json['count'], json['total']
+
+    def epg_search(self, args={"query":"","mode":0}, limit=0, start=0):
+        req_url = "%s/events/search.json?start=%s&limit=%s"%(self.base_url,start,limit)
+        json = self.JSON.decode(self.HTTP.POST(req_url,args))
+        return json['events'], json['count'], json['total']
+
+    def create_search_dict(self, query="",mode=0,channel_id="",use_title="true",use_subtitle="false",use_description="false"):
+        search_dict = {}
+        search_dict["query"] = query
+        search_dict["mode"] = mode
+        search_dict["channel_id"] = channel_id
+        search_dict["use_title"] = use_title
+        search_dict["use_subtitle"] = use_subtitle
+        search_dict["use_description"] = use_description
+
+    def get_osd(self):
+        url = "%s/osd.json"%(self.base_url)
+        return_val = {}
+        try:
+            osd = self.JSON.decode(self.HTTP.GET(url))
+        except:
+            return return_val
+        
+        try:
+            if osd['ChannelOsd']:
+                channel_number, channel_name = re.split("\s*",osd['ChannelOsd'],1)
+                return_val['channel']=self.get_channel_by_number(channel_number)
+        except:
+            pass
+        try:
+
+            if osd['ProgrammeOsd']:
+                return_val['events']=osd['ProgrammeOsd']
+        except:
+            pass
+        try:
+            if (osd['TextOsd']) and (len(osd['TextOsd']['items']) > 0):
+                
+                red = osd['TextOsd']['red']
+                yellow = osd['TextOsd']['yellow']
+                green = osd['TextOsd']['green']
+                blue = osd['TextOsd']['blue']
+                title = osd['TextOsd']['title']
+                # check if "normal" OSD menu is displayed (navigation by numbers is possible)
+                if re.match("^[0-9]{1,}\s*\w*", osd['TextOsd']['items'][0]['content'].lstrip()):
+                    print "Schema: <Nummer> <Eintrag>"
+                    for item in osd['TextOsd']['items']:
+                        number, value = re.split("\s*", item['content'].lstrip())
+                # check if normal navigation as in EPG
+                elif re.match("^\w{1,}\s*.*[0-9]{2}:[0-9]{2}",osd['TextOsd']['items'][0]['content'].lstrip()):
+                    print "Schema: Eintrag ohne Nummer"
+                    for item in osd['TextOsd']['items']:
+                        #print item['content']
+                        pass                
+
+                elif re.match("^\w{1,}.*:",osd['TextOsd']['items'][0]['content'].lstrip()):
+                    print "Settings/Selection"
+                    # This does not work because there are no values returned that are selected for editing :
+                    for item in osd['TextOsd']['items']:
+                        if item['is_selected'] == True:
+                            if re.match("^.*:.*[.*].*",item['content']):
+                                print "Editing value"
+            else:
+                print "No content"
+        except:
+            pass
+                    
+                    
+        return osd
+
+        
         
         
 
